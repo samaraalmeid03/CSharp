535865d36e58895f6618515bb0b9210c04be71e5
Samara Almeida <122751654+samaraalmeid03@users.noreply.github.com>
2023-04-23 17:04:15 -0300

    Create MEDIA.cs

---
+++ MEDIA.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Program {
+    public static void Main (string[] args) {
+        int n1, n2, n3, n4, n5, soma, media;
+
+        Console.Write("Digite o primeiro numero: ");
+        n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo numero: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o terceitro numero: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o quarto numero: ");
+        n4 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o quinto numero: ");
+        n5 = int.Parse(Console.ReadLine());
+
+        soma = n1 + n2 + n3 + n4 + n5;
+        media = soma / 5;
+
+        Console.WriteLine($"SOMA = {soma}");
+        Console.WriteLine($"MEDIA = {media}");
+        Console.ReadKey();
+    }
+}
