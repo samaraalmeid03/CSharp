68c242a91d0872e91284397a55fd47790760f1e6
Samara Almeida <122751654+samaraalmeid03@users.noreply.github.com>
2023-10-03 01:24:52 -0300

    Create Funcionario.cs

---
+++ AUMENTO_SALARIAL/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Projeto1
+{
+    class Funcionario
+    {   
+        //Declaração de atributos da classe Funcionario.
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        //Criação de métodos.
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double aumento = SalarioBruto * porcentagem / 100.0;
+            SalarioBruto += aumento;
+        }
+
+        //Formatação dos dados do funcionário que serão exibidos na tela.
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
