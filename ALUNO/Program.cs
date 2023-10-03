b29194da2629bf130c8f0a84a921a9ee6b4a6676
Samara Almeida <122751654+samaraalmeid03@users.noreply.github.com>
2023-10-03 01:20:04 -0300

    Create Program.cs

---
+++ ALUNO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno alun = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            alun.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno");
+            alun.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            alun.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            alun.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("NOTA FINAL = " + alun.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            
+            if (alun.Aprovado())
+            {
+                Console.WriteLine("\nAPROVADO!");
+            }
+            else
+            {
+                Console.WriteLine("\nREPROVADO!");
+                Console.WriteLine("FALTARAM " + alun.NotaFaltante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
