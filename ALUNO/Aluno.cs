e1f9ab65893cfe5739c6bca280e83940117de059
Samara Almeida <122751654+samaraalmeid03@users.noreply.github.com>
2023-10-03 01:21:16 -0300

    Create Aluno.cs

---
+++ ALUNO/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto1
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double NotaFaltante()
+        {
+            if (Aprovado()) {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
