1e06eb51c740a44a235cb7075714b88843455d9f
Samara Almeida <122751654+samaraalmeid03@users.noreply.github.com>
2023-10-03 01:17:46 -0300

    Update Triangulo.cs

--- AREA_TRIANGULO/Triangulo.cs
+++ AREA_TRIANGULO/Triangulo.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace Projeto1
 {
     class Triangulo
@@ -6,5 +7,11 @@ namespace Projeto1
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
