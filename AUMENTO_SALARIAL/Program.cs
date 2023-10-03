bf0fe2c0c0b7418dfad7d33c3430d95dc07bfeee
Samara Almeida <122751654+samaraalmeid03@users.noreply.github.com>
2023-10-03 01:23:11 -0300

    Create Program.cs

---
+++ AUMENTO_SALARIAL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            //Entrada de dados.
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Exibição de informações referentes ao funcionario.
+            Console.WriteLine();
+            Console.Write("Funcionario: " + func);
+
+            //Entrada do percentual de aumento do salario e chamada da função AumentarSalario().
+            Console.WriteLine();
+            Console.Write("\nDigite a porcentagem para aumentar o salario: ");
+            double pcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(pcent);
+
+            //Impressão dos dados atualizados.
+            Console.WriteLine();
+            Console.Write("Dados atualizados: " + func);
+        }
+    }
+}
